126215e255ae4d523a660e9b2f540834088cc372
Гарчукова Полина <91340112+Gen1ot@users.noreply.github.com>
2024-01-10 00:03:57 +0700

    Create Lab3.c

---
+++ Lab3.c
@@ -0,0 +1,17 @@
+#include <linux/module.h> 
+#include <linux/init.h> 
+#include <linux/printk.h> 
+ 
+MODULE_DESCRIPTION("tsulab"); 
+MODULE_AUTHOR("linanung"); 
+MODULE_LICENSE("GPL"); 
+ 
+int init_module(void){   
+  pr_info("Welcome to the Tomsk State University\n");
+  
+  return 0; 
+} 
+ 
+void cleanup_module(void){ 
+    pr_info("Tomsk State University forever!\n");
+}
